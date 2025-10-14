45dade0a7bee75540b5206f3ecd88912facd73d8
Homka <153567922+Homkaw@users.noreply.github.com>
2025-10-15 03:07:00 +0400

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -27,6 +27,36 @@ const projects = [
     img: "./img/portfolio3.jpg",
     desc: "Учебный проект с классным дизайном и версткой.",
     link: "https://homkaw.github.io/cookie/"
+  },
+  {
+    title: "Сайт по заказу реплик картин",
+    img: "./img/portfolio4.jpg",
+    desc: "Проект был создан в свободное время для тренировки",
+    link: "https://homkaw.github.io/cookie/"
+  },
+  {
+    title: "Сайт на 23 февраля",
+    img: "./img/portfolio5.jpg",
+    desc: "Проект был создан в свободное время для праздника",
+    link: "https://homkaw.github.io/23/"
+  },
+  {
+    title: "Сайт разработки игр",
+    img: "./img/portfolio6.jpg",
+    desc: "Учебный проект для сокурсника.",
+    link: "https://homkaw.github.io/igry/"
+  },
+  {
+    title: "Сайт Астория",
+    img: "./img/portfolio7.jpg",
+    desc: "Учебный проект созданный на 2 курсе.",
+    link: "https://homkaw.github.io/astoria/"
+  },
+  {
+    title: "Сайт Сказочный лес",
+    img: "./img/portfolio8.jpg",
+    desc: "Проект для тренировки",
+    link: "https://homkaw.github.io/fairy/"
   }
 ];
 
